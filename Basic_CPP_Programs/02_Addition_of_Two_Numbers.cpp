7f8088aa0c475e4ac0d671ee35d85f96f1674631
Sakshipatil21 <102903027+Sakshipatil21@users.noreply.github.com>
2022-11-24 20:44:27 +0530

    Basic Program

---
+++ Basic_CPP_Programs/02_Addition_of_Two_Numbers.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+        int Num1 = 0, Num2 = 0, Sum = 0;
+
+        cout << "\n Enter First Number = ";
+        cin >> Num1;
+
+        cout << "\n Enter Second Number = ";
+        cin >> Num2;
+
+        Sum = Num1 + Num2;
+
+        cout << "\n Addition Of " << Num1 << " and " << Num2 << " is  = " << Sum << endl;
+
+        cout << "\n Thanks !!!\n" << endl;
+
+        getch();
+        return 0;
+}
